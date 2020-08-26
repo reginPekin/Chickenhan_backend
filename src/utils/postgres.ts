b4c1343815c694b1dfde84302e2c0c4e111a39b4
reginPekin <reginbegin@gmail.com>
2020-08-26 23:09:24 +0300

    Create three more tables

--- src/utils/postgres.ts
+++ src/utils/postgres.ts
@@ -18,19 +18,47 @@ async function createTables() {
     );`,
     `CREATE TABLE authGoogle
     (
-      user_id SERIAL UNIQUE NOT NULL,
+      user_id INTEGER UNIQUE NOT NULL,
       googleToken VARCHAR(255) PRIMARY KEY NOT NULL
     );`,
     `CREATE TABLE authFacebook
     (
-      user_id SERIAL UNIQUE NOT NULL,
+      user_id INTEGER UNIQUE NOT NULL,
       facebookToken VARCHAR(255) PRIMARY KEY NOT NULL
     );`,
-    `CREATE TABLE authMail
+    `CREATE TABLE authLogin
     (
-      user_id SERIAL UNIQUE NOT NULL,
+      user_id INTEGER UNIQUE NOT NULL,
       password VARCHAR(255) NOT NULL,
       login VARCHAR(32) PRIMARY KEY NOT NULL
+    );`,
+    `CREATE TABLE messages
+    (
+      message_id BIGSERIAL PRIMARY KEY UNIQUE NOT NULL,
+      author_id INTEGER NOT NULL,
+      chat_id INTEGER NOT NULL,
+      text TEXT,
+      date VARCHAR(32),
+      pictures BIGINT[]
+    );`,
+    `CREATE TABLE chats
+    (
+      chat_id BIGSERIAL PRIMARY KEY UNIQUE NOT NULL,
+      type VARCHAR(32) NOT NULL,
+      avatar VARCHAR(255) NOT NULL,
+
+      last_date_message VARCHAR(32),
+      last_message TEXT,
+
+      name VARCHAR(32) NOT NULL,
+      user_count INTEGER NOT NULL,
+
+      opponent_id INTEGER
+    );`,
+    `CREATE TABLE usersChats
+    (
+      user_id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+      chats BIGSERIAL[]
     );`,
   ];
 
