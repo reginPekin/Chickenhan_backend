5088f709e4537cc539aad516572485a6f60add1a
reginPekin <reginbegin@gmail.com>
2020-08-26 23:10:28 +0300

    Add CORS

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,28 @@
 import * as express from 'express';
 import * as path from 'path';
 import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
 
-import { app, get, post, patch } from './utils/express';
+import { app, get, post, patch, del } from './utils/express';
 
 import { API_PORT } from './config';
 
 import { initPostgres } from './utils/db';
 
 import { getMe, getUser, updateUser, addUserByLogin } from './api/user';
-
 import {
   authUserByFacebook,
   authUserByGoogle,
-  authUserByMail,
-  signUpUserByMail,
+  authUserByLogin,
+  signUpUserByLogin,
   signUpUserByGoogle,
   signUpUserByFacebook,
 } from './api/auth';
+import { addMessage, getMessageById, deleteMessageById } from './api/messages';
+import { addChat, updateChatById, getChatById } from './api/chats';
+
+app.use(cors());
+app.options('*', cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -33,11 +38,19 @@ post('/users/login', addUserByLogin);
 
 post('/auth/facebook', authUserByFacebook);
 post('/auth/google', authUserByGoogle);
-post('/auth/mail', authUserByMail);
+post('/auth/login', authUserByLogin);
 
-post('/auth/new/mail', signUpUserByMail);
+post('/auth/new/login', signUpUserByLogin);
 post('/auth/new/google', signUpUserByGoogle);
 post('/auth/new/facebook', signUpUserByFacebook);
+
+get(`/messages`, getMessageById);
+del(`/messages`, deleteMessageById);
+post(`/messages/:chat_id`, addMessage);
+
+get('/chats', getChatById);
+post('/chats', addChat);
+patch('/chats', updateChatById);
 
 initPostgres();
 
