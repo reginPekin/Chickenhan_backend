5ae2b22c1d0acf043cc37a74c564688c37e08786
reginPekin <reginbegin@gmail.com>
2020-08-31 23:28:24 +0300

    Get token inside express fnctions

--- src/utils/express.ts
+++ src/utils/express.ts
@@ -2,33 +2,67 @@ import * as express from 'express';
 
 import { Server, parseToServer } from './server';
 import { API_PATH } from '../config';
+import { User, getUserByToken } from '../lib/user';
 
 export const app = express();
 
-export function get(destination: string, callback: (server: Server) => any) {
-  return app.get(API_PATH + destination, (req, res) => {
+async function extractUser(token: string): Promise<User | undefined> {
+  if (!token) {
+    return undefined;
+  }
+
+  try {
+    const user = await getUserByToken(token);
+    return user;
+  } catch {
+    return undefined;
+  }
+}
+
+export function get(
+  destination: string,
+  callback: (server: Server, user?: User) => any,
+) {
+  return app.get(API_PATH + destination, async (req, res) => {
     const server = parseToServer(req, res);
-    callback(server);
+
+    const user = await extractUser(server.headers.token);
+    callback(server, user);
   });
 }
 
-export function post(destination: string, callback: (server: Server) => any) {
-  return app.post(API_PATH + destination, (req, res) => {
+export function post(
+  destination: string,
+  callback: (server: Server, user?: User) => any,
+) {
+  return app.post(API_PATH + destination, async (req, res) => {
     const server = parseToServer(req, res);
-    callback(server);
+
+    const user = await extractUser(server.headers.token);
+    callback(server, user);
   });
 }
 
-export function patch(destination: string, callback: (server: Server) => any) {
-  return app.patch(API_PATH + destination, (req, res) => {
+export function patch(
+  destination: string,
+  callback: (server: Server, user?: User) => any,
+) {
+  return app.patch(API_PATH + destination, async (req, res) => {
     const server = parseToServer(req, res);
-    callback(server);
+
+    const user = await extractUser(server.headers.token);
+    callback(server, user);
   });
 }
 
-export function del(destination: string, callback: (server: Server) => any) {
-  return app.delete(API_PATH + destination, (req, res) => {
+export function del(
+  destination: string,
+  callback: (server: Server, user?: User) => any,
+) {
+  return app.delete(API_PATH + destination, async (req, res) => {
     const server = parseToServer(req, res);
-    callback(server);
+
+    const user = await extractUser(server.headers.token);
+    callback(server, user);
   });
 }
